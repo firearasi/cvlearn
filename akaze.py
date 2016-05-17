6209491503c5fb13819360f015baf61396b5282b
Cheng Yan <firearasi@qq.com>
2016-05-17 16:41:11 +0800

    a

--- akaze.py
+++ akaze.py
@@ -11,35 +11,45 @@ import matplotlib.pyplot as plt
 
   #%%
 cv2.ocl.setUseOpenCL(False)
-img = cv2.imread('data/starbucks.jpg')
-img1=cv2.imread('data/starbucks1.jpg')
+img1 = cv2.imread('data/starbucks.jpg')
+img2=cv2.imread('data/starbucks1.jpg')
 
 # Initiate STAR detector
 fd = cv2.AKAZE_create()
-kp,des=fd.detectAndCompute(img,None)
 kp1,des1=fd.detectAndCompute(img1,None)
+kp2,des2=fd.detectAndCompute(img2,None)
 
-matcher=cv2.BFMatcher(cv2.NORM_HAMMING,crossCheck=True)
-#FLANN_INDEX_LSH=6
-#flann_params= dict(algorithm = FLANN_INDEX_LSH,
-#                   table_number = 6, # 12
-#                  key_size = 12,     # 20
-#                   multi_probe_level = 1) #2
-#matcher=cv2.FlannBasedMatcher(flann_params, {}) 
-matches=matcher.match(des,des1)
+#matcher=cv2.BFMatcher(cv2.NORM_HAMMING,crossCheck=True) crossCheck = True only when k=1
+matcher=cv2.BFMatcher(cv2.NORM_HAMMING,crossCheck=False)
 
-matches = sorted(matches, key = lambda x:x.distance)
+matches=matcher.knnMatch(des1,des2,k=2)
+
+matchesMask = [[0,0] for i in range(len(matches))]
+
+# ratio test as per Lowe's paper
+num_good_matches = 0
+for i,(m,n) in enumerate(matches):
+    if m.distance < 0.6*n.distance:
+        matchesMask[i]=[1,0]
+        num_good_matches += 1
+
+print("Num of good matches:",num_good_matches)
 
 
-img2 = cv2.drawMatches(img,kp,img1,kp1,matches[:20],None, flags=2)
+draw_params = dict(matchColor = (0,255,0),
+                   singlePointColor = (255,0,0),
+                   matchesMask = matchesMask,
+                   flags = 0)
+
+img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
 
 try:
   cv2.namedWindow("Pic",cv2.WINDOW_NORMAL)
-  cv2.imshow("Pic",img2)
+  cv2.imshow("Pic",img3)
   cv2.resizeWindow("Pic",1920,1080)
 
   if cv2.waitKey(0)  ==ord('q'):
     cv2.destroyAllWindows()
 except:
   pass
-cv2.imwrite("output/starbucks_match.png",img2)
+cv2.imwrite("output/starbucks_match.png",img3)
--- flannmatcher.py
+++ flannmatcher.py
@@ -11,17 +11,14 @@ import matplotlib.pyplot as plt
 
   #%%
 cv2.ocl.setUseOpenCL(False)
-img = cv2.imread('data/box.png')
-img1=cv2.imread('data/box_in_scene.png')
+img1 = cv2.imread('data/box.png')
+img2=cv2.imread('data/box_in_scene.png')
 
 # Initiate STAR detector
 fd=cv2.ORB_create()
 #fd = cv2.AKAZE_create()
-kp,des=fd.detectAndCompute(img,None)
 kp1,des1=fd.detectAndCompute(img1,None)
-
-#matcher=cv2.BFMatcher(cv2.NORM_HAMMING2,crossCheck=True)
-#matcher=cv2.BFMatcher(cv2.NORM_L2,crossCheck=True)
+kp2,des2=fd.detectAndCompute(img2,None)
 
 
 FLANN_INDEX_LSH    = 6
@@ -31,29 +28,34 @@ flann_params= dict(algorithm = FLANN_INDEX_LSH,
                    multi_probe_level = 1) #2
 matcher=cv2.FlannBasedMatcher(flann_params,{}) 
 
-matches=matcher.knnMatch(des,des1,k=2)
+matches=matcher.knnMatch(des1,des2,k=2)
 
 #Need to draw only good matches, so create a mask
 matchesMask = [[0,0] for i in range(len(matches))]
 
 # ratio test as per Lowe's paper
+num_good_matches = 0
 for i,(m,n) in enumerate(matches):
-    if m.distance < 0.7*n.distance:
+    if m.distance < 0.58*n.distance:
         matchesMask[i]=[1,0]
+        num_good_matches += 1
+
+print("Num of good matches:",num_good_matches)
+
 
 draw_params = dict(matchColor = (0,0,255),
                    singlePointColor = (255,0,0),
                    matchesMask = matchesMask,
                    flags = 0)
 
-img2 = cv2.drawMatchesKnn(img,kp,img1,kp1,matches,None,**draw_params)
+img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
 
 
 
 
 try:
   cv2.namedWindow("Pic",cv2.WINDOW_NORMAL)
-  cv2.imshow("Pic",img2)
+  cv2.imshow("Pic",img3)
   cv2.resizeWindow("Pic",1920,1080)
 
   if cv2.waitKey(0)  ==ord('q'):
@@ -61,4 +63,4 @@ try:
 except:
   pass
 
-cv2.imwrite("output/box_match.png",img2)
+cv2.imwrite("output/box_match.png",img3)
